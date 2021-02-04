65027bc888503b16a09843e35a2811d0a044c5e8
Barbara Diaz <barbarad91@gmail.com>
2021-02-04 21:21:19 +0100

    Render movie details

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -8,6 +8,13 @@ router.get("/", (req, res) => {
   Movie.find()
     .select("title image")
     .then((allMovies) => res.render("movies", { allMovies }))
+    .catch((err) => console.err(err))
 })
 
+router.get("/:movieId", (req, res) => {
+  //res.send(req.params)
+  Movie.findById(req.params.movieId).then((movie) => {
+    res.render("movie-detail", movie)
+  })
+})
 module.exports = router
