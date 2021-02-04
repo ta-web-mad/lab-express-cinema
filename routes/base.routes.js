5b657b2f1a535400d31cc50a4ac02f59e0145278
montse <montserrat.mosqueda@gmail.com>
2021-02-04 22:34:29 +0100

    commit

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,35 @@
 const express = require('express')
 const router = express.Router()
 
+
+const Movie = require('./../models/Movie.model')
+
+
 // Endpoints
 router.get('/', (req, res) => res.render('index'))
 
 
-module.exports = router
+
+router.get('/movies-list', (req, res) => {
+
+        Movie
+                .find()
+                .select('title')
+                .select('image')
+                .then(movies => res.render('movies-list', { movies}))
+                .catch(err => console.log('ERROR:', err))
+})
+
+
+
+
+router.get('/movies/description/:id', (req, res) => {
+        const movieid = req.params.id
+        Movie
+        .findById(movieid)
+        .then(movie => res.render('movies-description', movie))
+        .catch(err => console.log(err))          
+
+})
+
+        module.exports = router
