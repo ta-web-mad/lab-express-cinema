3658ccd46f00856ec9b24b9a0aa3eeee099aa503
Alejandro <evergreenterras@hotmail.com>
2021-02-04 22:29:30 +0100

    almost done

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const Movie = require('../models/Movie.model')
+
 
 // Endpoints
 router.get('/', (req, res) => res.render('index'))
+
+
+
+
+// listado de peliculas
+router.get('/movies', (req, res)=>{
+
+    Movie
+        .find()
+        .select('title')
+        .select('image')
+        .then(movies => {
+            
+
+            res.render('movies-list', {movies})})
+        .catch(err => console.log('Error', err))
+} )
+
+
+// detalles de la pelicula
+router.get('/detalles/:movieid', (req, res) => {
+
+    const movie_id = req.params.movieid
+    
+    Movie
+        .findById(movie_id)
+        .then(themovie => {
+            res.render('movie-details', themovie)})
+        
+        .catch(err => console.log('Error', err))
+})
 
 
 module.exports = router
